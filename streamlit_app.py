3576070f2dc187cc1f5e3aec17ab674aa941480e
jinyech <99807007+jinyech@users.noreply.github.com>
2022-02-19 10:31:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ import streamlit as st
 简政是一个提供全面政策信息、提供专业政策建议的一体化平台。
 我们的愿景是“让数据意识普及，让智能工具惠民，让政府信息更加亲民”
 """
-wordword=open('浙江省保障性住房解疑.txt','r')
+wordword=open("浙江省保障性住房解疑.txt","r",encoding='utf-8').read()
 if st.button('保障性住房解疑'):
    st.write(wordword)

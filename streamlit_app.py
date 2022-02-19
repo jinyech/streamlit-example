c6686ed1f86cf7441704ca08ebbc8bf1ad04b957
jinyech <99807007+jinyech@users.noreply.github.com>
2022-02-19 14:50:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import json
+
 """
 # Welcome to 简政!
 简政是一个提供全面政策信息、提供专业政策建议的一体化平台。
@@ -12,7 +12,4 @@ import json
 wordword=open("浙江省保障性住房解疑.txt","r",encoding='utf-8').read()
 if st.button('保障性住房解疑'):
    st.write(wordword)
-#o=open('浙江省人民政府-住房搜索结果.json','r',encoding='utf-8').read()
-#o=json.load(o)
 
-st.dataframe({'k':'k'})
